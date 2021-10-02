154427e77b05aa6aab547e2d1e2b4a7673de68ca
xianyue <43592186+xianyueer@users.noreply.github.com>
2021-10-02 23:47:27 +0800

    代码小优化下

--- LeetCode/竞赛/双周赛/51-100/双周赛-62/5873. 考试的最大困扰度.py
+++ LeetCode/竞赛/双周赛/51-100/双周赛-62/5873. 考试的最大困扰度.py
@@ -3,37 +3,30 @@ class Solution:
         # O(n) 或者 O(logn)
         n = len(answerKey)
         preCnt = [0] * n
+        
+        def binarySearch(l, r):
+            def test(m):
+                for i in range(n-m+1):
+                    if preCnt[i+m-1] - (preCnt[i-1] if i != 0 else 0) <= k:
+                        return True
+                return False
+
+            while l < r:
+                m = l + (r-l >> 1) + 1
+                if test(m):
+                    l = m
+                else:
+                    r = m-1
+            return l
+
+        preCnt[0] = 0
         for i in range(n):
             preCnt[i] = (preCnt[i-1] if i > 0 else 0) + (answerKey[i] == 'T')
-        l = 0
-        r = n
+        a = binarySearch(0, n)
 
-        def test(m):
-            for i in range(n-m+1):
-                if preCnt[i+m-1] - (preCnt[i-1] if i != 0 else 0) <= k:
-                    return True
-            return False
-
-        while l < r:
-            m = l + (r-l >> 1) + 1
-            if test(m):
-                l = m
-            else:
-                r = m-1
-        a = l
-
-        preCnt = [0] * n
+        preCnt[0] = 0
         for i in range(n):
             preCnt[i] = (preCnt[i-1] if i > 0 else 0) + (answerKey[i] == 'F')
-        l = 0
-        r = n
+        b = binarySearch(0, n)
 
-        while l < r:
-            m = l + (r-l >> 1) + 1
-            if test(m):
-                l = m
-            else:
-                r = m-1
-        b = l
-        print(a, b)
         return max(a, b)
